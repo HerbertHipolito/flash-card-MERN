6cd522d58f8d70a7863badc62eeb93ae8d29b1c7
Herbert Hipólito <hipolito160@gmail.com>
2023-01-19 10:58:00 -0300

    header modified

--- server/server.js
+++ server/server.js
@@ -17,11 +17,14 @@ app.use(cors({
 }));
 */
 
-app.use(cors({
-    origin:'*',
-    credentials:true
-}));
-
+app.use((req,res,next)=>{
+    //res.header('Access-Control-Allow-Origin','*');
+    //res.header('Access-Control-Allow-Headers','*');
+    res.header('Access-Control-Allow-Origin',"*")
+    res.header('Access-Control-Allow-Headers','*');
+    cors();
+    next()
+})
 
 const connectDB = require('./config/dbconfig');
 
